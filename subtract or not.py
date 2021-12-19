ba8b48d960fa0044a8189be7ecc7a804b9e5f4f0
Abhishek7668 <92740857+Abhishek7668@users.noreply.github.com>
2021-12-19 21:02:34 +0530

    Add files via upload

---
+++ subtract or not.py
@@ -0,0 +1,8 @@
+a = int (input( " enter first no " ))
+b = int ( input ( " enter secound no "))
+if (a>=b):
+    print ( "no is subtract",b)
+    subtract = (a-b)
+    print ( " anser is " ,subtract)
+else:
+    print(" no is not subtract",a )
